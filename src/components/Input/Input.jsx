c4f4f9ea7efd91e148bd1e5bf0b1e56c00e1e5f3
gonakano <g.nakano555@gmail.com>
2022-01-02 13:04:27 +0900

    created shared input component with styling

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useFormContext, get } from "react-hook-form";
+
+import "./input.scss";
+
+const propTypes = {
+  // hook-form register name
+  name: PropTypes.string,
+
+  label: PropTypes.string,
+  register: PropTypes.func,
+  layout: PropTypes.string,
+};
+
+const Input = ({ name, label, layout }) => {
+  const { register, formState } = useFormContext();
+  const error = get(formState.errors, name);
+
+  return (
+    <div className={`input-container ${layout}`}>
+      {label && <label className="title-font mb-2">{label}</label>}
+      <input {...register(name)} className="base-font py-1 px-3" />
+      {error && <p className="error-font mb-0 mt-1">{error.message}</p>}
+    </div>
+  );
+};
+
+Input.propTypes = propTypes;
+Input.defaultProps = {
+  label: "",
+  register: () => null,
+  layout: "",
+};
+
+export default Input;
